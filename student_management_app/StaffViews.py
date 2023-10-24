334f449e6aaca70117bba9481ca4690e3d6044b6
Francisco Passalacqua <87296774+WhoIsFrancis@users.noreply.github.com>
2023-10-24 20:18:27 -0300

    Add -> staff puede enviar mensaje para retirarse y se implementa el feedback

--- student_management_app/StaffViews.py
+++ student_management_app/StaffViews.py
@@ -1,9 +1,11 @@
 import json
-from django.http import JsonResponse, HttpResponse
+from django.http import HttpResponseRedirect, JsonResponse, HttpResponse
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib import messages
 
-from student_management_app.models import Attendance, AttendanceReport, SessionYearModel, Students, Subjects
+from student_management_app.models import Attendance, AttendanceReport, FeedBackStaffs, LeaveReportStaff, SessionYearModel, Staffs, Students, Subjects
 
 def staff_home(request):
     return render(request,'staff_template/staff_home_template.html')
@@ -115,3 +117,47 @@ def save_updateattendance_data(request):
         return HttpResponse("OK")
     except:
         return HttpResponse("ERR")
+
+def staff_apply_leave(request):
+    staff_obj=Staffs.objects.get(admin=request.user.id )
+    leave_data=LeaveReportStaff.objects.filter(staff_id=staff_obj)
+    return render(request, "staff_template/staff_apply_leave.html", {"leave_data": leave_data})
+
+def staff_apply_leave_save(request):
+    if request.method != "POST":
+        return HttpResponseRedirect(reverse("staff_apply_leave"))
+    else:
+        leave_date=request.POST.get("leave_date")
+        leave_msg=request.POST.get("leave_msg")
+
+        staff_obj= Staffs.objects.get(admin=request.user.id)
+        try:
+            leave_report=LeaveReportStaff(staff_id=staff_obj, leave_date=leave_date, leave_message=leave_msg, leave_status=0)
+            leave_report.save()
+            messages.success(request, "Succesfully applied for leave")
+            return HttpResponseRedirect(reverse("staff_apply_leave"))
+        except:
+            messages.error(request, "Failed to apply for leave")
+            return HttpResponseRedirect(reverse("staff_apply_leave"))
+
+
+def staff_feedback(request):
+    staff_id= Staffs.objects.get(admin=request.user.id)
+    feedback_data= FeedBackStaffs.objects.filter(staff_id=staff_id)
+    return render(request, "staff_template/staff_feedback.html", {"feedback_data": feedback_data})
+
+def staff_feedback_save(request):
+    if request.method!="POST":
+        return HttpResponseRedirect(reverse("staff_feedback_save"))
+    else:
+        feedback_msg=request.POST.get("feedback_msg")
+
+        staff_obj=Staffs.objects.get(admin=request.user.id)
+        try:
+            feedback=FeedBackStaffs(staff_id=staff_obj,feedback=feedback_msg,feedback_reply="")
+            feedback.save()
+            messages.success(request, "Successfully Sent Feedback")
+            return HttpResponseRedirect(reverse("staff_feedback"))
+        except:
+            messages.error(request, "Failed To Send Feedback")
+            return HttpResponseRedirect(reverse("staff_feedback"))
